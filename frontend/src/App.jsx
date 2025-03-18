6b02f7733beefa9563bba96eb1dcff866952a426
Victor Valero <vvalerolopez.eb@iesesteveterradas.cat>
2025-03-18 23:24:35 +0100

    [ IN PROGRESS ] Files moved into front and started pokemon card

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as AppRoutes } from "react-router-dom";
-import AuthRoutes from './components/AuthRoutes';
-import './App.css';
+import AuthRoutes from "./components/AuthRoutes";
+import "./App.css";
 
 function App() {
-    return (
-      <AppRoutes>
-        <AuthRoutes />
-      </AppRoutes>
-    );
+  return (
+    <AppRoutes>
+      <AuthRoutes />
+    </AppRoutes>
+  );
 }
 
 export default App;
--- frontend/src/components/AuthRoutes.jsx
+++ frontend/src/components/AuthRoutes.jsx
@@ -1,52 +1,95 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Login from './Login';
-import Register from './Register';
-import User from './User'
+import Login from "./Login";
+import Register from "./Register";
+import User from "./User";
 import AdminPanel from "./AdminPanel";
 import TeamBuilder from "./TeamBuilder";
+import TeamMaker from "./TeamMaker";
 
 function AuthRoutes() {
-    
-    const RootRedirect = () => {
-        const token = localStorage.getItem('token');
-        return <Navigate to={token ? "/user" : "/auth/login"} />;
-    };
-    
-    // Protege rutas privadas (solo accedibles con token)
-    const PrivateRoute = ({ children }) => {
-        const token = localStorage.getItem('token');
-        return token ? children : <Navigate to="/auth/login" />;
-    };
+  const RootRedirect = () => {
+    const token = localStorage.getItem("token");
+    return <Navigate to={token ? "/user" : "/auth/login"} />;
+  };
 
-    // Redirige a /user si el usuario está autenticado desde /auth
-    const AuthRedirectRoute = () => {
-        const token = localStorage.getItem('token');
-        return token ? <Navigate to="/user"/> : <Navigate to="/auth/login" />;
-    };
-    
-    // Evita que los usuarios autenticados accedan a login/register
-    const AuthRoute = ({ children }) => {
-        const token = localStorage.getItem('token');
-        return token ? <Navigate to="/user" /> : children;
-    };
-    
-    // Evita que los usuarios autenticados accedan al admin panel
-    const AdminPanelRoute = ({ children }) => {
-        const token = localStorage.getItem('token');
-        return token ? children : <Navigate to="/auth/login" />;
-    };
+  // Protege rutas privadas (solo accedibles con token)
+  const PrivateRoute = ({ children }) => {
+    const token = localStorage.getItem("token");
+    return token ? children : <Navigate to="/auth/login" />;
+  };
 
-    return (
-        <Routes>
-            <Route path="/" element={<RootRedirect />} />
-            <Route path="/auth" element={<AuthRedirectRoute></AuthRedirectRoute>} />
-            <Route path="/auth/login" element={<AuthRoute><Login /></AuthRoute>} />
-            <Route path="/auth/register" element={<AuthRoute><Register /></AuthRoute>} />
-            <Route path="/user" element={<PrivateRoute><User /></PrivateRoute>} />
-            <Route path="/admin" element={<AdminPanelRoute><AdminPanel /></AdminPanelRoute>} />
-            <Route path="/teambuilder" element={<PrivateRoute><TeamBuilder /></PrivateRoute>} />
-        </Routes>
-    );
-};
+  // Redirige a /user si el usuario está autenticado desde /auth
+  const AuthRedirectRoute = () => {
+    const token = localStorage.getItem("token");
+    return token ? <Navigate to="/user" /> : <Navigate to="/auth/login" />;
+  };
+
+  // Evita que los usuarios autenticados accedan a login/register
+  const AuthRoute = ({ children }) => {
+    const token = localStorage.getItem("token");
+    return token ? <Navigate to="/user" /> : children;
+  };
+
+  // Evita que los usuarios autenticados accedan al admin panel
+  const AdminPanelRoute = ({ children }) => {
+    const token = localStorage.getItem("token");
+    return token ? children : <Navigate to="/auth/login" />;
+  };
+
+  return (
+    <Routes>
+      <Route path="/" element={<RootRedirect />} />
+      <Route path="/auth" element={<AuthRedirectRoute></AuthRedirectRoute>} />
+      <Route
+        path="/auth/login"
+        element={
+          <AuthRoute>
+            <Login />
+          </AuthRoute>
+        }
+      />
+      <Route
+        path="/auth/register"
+        element={
+          <AuthRoute>
+            <Register />
+          </AuthRoute>
+        }
+      />
+      <Route
+        path="/user"
+        element={
+          <PrivateRoute>
+            <User />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/admin"
+        element={
+          <AdminPanelRoute>
+            <AdminPanel />
+          </AdminPanelRoute>
+        }
+      />
+      <Route
+        path="/teambuilder"
+        element={
+          <PrivateRoute>
+            <TeamBuilder />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/teammaker"
+        element={
+          <PrivateRoute>
+            <TeamMaker />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
+}
 
 export default AuthRoutes;
