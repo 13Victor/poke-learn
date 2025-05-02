a9896f76348c70f76179de5170625bae0c95730b
Victor Valero <vvalerolopez.eb@iesesteveterradas.cat>
2025-05-02 09:40:10 +0200

    [ CSS ] Category move centered

--- frontend/src/components/TeamMaker/MoveRow.jsx
+++ frontend/src/components/TeamMaker/MoveRow.jsx
@@ -26,25 +26,27 @@ const MoveRow = memo(({ move, onClick, isEven }) => {
         </div>
       </td>
       <td>
-        <Tippy
-          content={
-            <div className="category-tooltip-content">
-              <img src={`/assets/move-category/${move.category}.png`} />
-              <p>{move.category}</p>
-            </div>
-          }
-          placement="top"
-          animation="scale"
-          theme={`category-tooltip-${move.category.toLowerCase()}`}
-          delay={[300, 100]}
-          arrow={true}
-        >
-          <img
-            src={`/assets/move-category/${move.category}.png`}
-            title={move.category}
-            className={`category-icon category-${move.category.toLowerCase()}`}
-          />
-        </Tippy>
+        <div className="category-cell">
+          <Tippy
+            content={
+              <div className="category-tooltip-content">
+                <img src={`/assets/move-category/${move.category}.png`} />
+                <p>{move.category}</p>
+              </div>
+            }
+            placement="top"
+            animation="scale"
+            theme={`category-tooltip-${move.category.toLowerCase()}`}
+            delay={[300, 100]}
+            arrow={true}
+          >
+            <img
+              src={`/assets/move-category/${move.category}.png`}
+              title={move.category}
+              className={`category-icon category-${move.category.toLowerCase()}`}
+            />
+          </Tippy>
+        </div>
       </td>
       <td>{move.basePower || "-"}</td>
       <td>{move.accuracy || "-"}</td>
